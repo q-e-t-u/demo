8396e336f18f215bb4fb847bb5e74c410fb7addd
q-e-t-u <59313139+q-e-t-u@users.noreply.github.com>
2021-03-23 18:04:20 +0530

    rest-de

---
+++ rest-de/src/main/java/com/rest/restde/RestDeApplication.java
@@ -0,0 +1,13 @@
+package com.rest.restde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDeApplication.class, args);
+	}
+
+}
---
+++ rest-de/src/test/java/com/rest/restde/RestDeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.restde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
